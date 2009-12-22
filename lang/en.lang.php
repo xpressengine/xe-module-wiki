239fadd2b2cc8406f07fa031f959e0c2097b55fd
haneul <syhan@cs.washington.edu>
2009-12-22 02:53:27 +0000

    English translation
    git-svn-id: http://xe-wiki.googlecode.com/svn/trunk/modules/wiki@14 93e89afd-8622-bd68-7444-12f807e44a33

--- lang/en.lang.php
+++ lang/en.lang.php
@@ -11,12 +11,12 @@
     $lang->cmd_wiki_list = 'List';
     $lang->cmd_view_info = 'Wiki Information';
     $lang->use_comment = 'Use Comments';
-    $lang->cmd_view_document_list = '문서 목록';
-    $lang->cmd_view_document_tree = '계층 보기';
+    $lang->cmd_view_document_list = 'Documents List';
+    $lang->cmd_view_document_tree = 'Documents Tree';
     $lang->about_use_comment = 'You may configure to enable comments.';
     $lang->contributors = 'Contributors';
     $lang->notice_old_revision = 'This is an old revision';
     $lang->restore = 'Restore';
-    $lang->arrange_list = '목록 갱신';
-    $lang->about_arrange_list = '데이터 이전하였거나 의도하지 않은 경우에 의해 제목 Alias가 설정되지 않아서 위키 글을 찾아갈 수 없는 문서들을 일괄적으로 제목 Alias를 설정해줍니다.';
+    $lang->arrange_list = 'Refresh Alias';
+    $lang->about_arrange_list = "It creates aliases for documents which do not have aliases for title due to data migration or unexpected errors.";
 ?>
