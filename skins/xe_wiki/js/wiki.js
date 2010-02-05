4c79ecf9534d8a894384a2c6f8b8dba377c19317
haneul <syhan@cs.washington.edu>
2010-02-05 12:50:13 +0000

    #18667580 : additional modification
    git-svn-id: http://xe-wiki.googlecode.com/svn/trunk/modules/wiki@18 93e89afd-8622-bd68-7444-12f807e44a33

--- skins/xe_wiki/js/wiki.js
+++ skins/xe_wiki/js/wiki.js
@@ -1,11 +1,3 @@
-function moveHistory(fo_obj) {
-    if(!fo_obj.entry.value) return;
-    var url = request_uri;
-    if(typeof(xeVid)!='undefined') url = url.setQuery('vid',xeVid);
-    url = url.setQuery('mid',current_mid).setQuery('entry',fo_obj.entry.value);
-    location.href=url;
-}
-
 function viewHistory(history_srl) {
     var zone = jQuery('#historyContent'+history_srl);
     if(zone.css('display')=='block') zone.css('display','none');
