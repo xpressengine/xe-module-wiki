0cf9b88bd6d8b78d4b2be69582f013d8bb35a0ae
ChanMyeong <dece24@gmail.com>
2013-03-18 01:56:25 +0000

    XE core 1.7 optimization. Start.
    git-svn-id: http://xe-wiki.googlecode.com/svn/branches/modules/wiki/1.7.0@326 93e89afd-8622-bd68-7444-12f807e44a33

---
+++ tpl/js/wiki_admin.js
@@ -0,0 +1,52 @@
+/**
+ * @file   modules/wiki/js/wiki_admin.js
+ * @author zero (zero@nzeo.com)
+ * @brief  wiki 모듈의 관리자용 javascript
+ **/
+
+
+/* after creation wiki Module */
+function completeInsertWiki(ret_obj) {
+    var error = ret_obj['error'];
+    var message = ret_obj['message'];
+
+    var page = ret_obj['page'];
+    var module_srl = ret_obj['module_srl'];
+
+    alert(message);
+
+    var url = current_url.setQuery('act','dispWikiAdminInsertWiki');
+    if(module_srl) url = url.setQuery('module_srl',module_srl);
+    if(page) url.setQuery('page',page);
+    location.href = url;
+}
+
+/*  after deleting wiki Module */
+function completeDeleteWiki(ret_obj) {
+    var error = ret_obj['error'];
+    var message = ret_obj['message'];
+    var page = ret_obj['page'];
+    alert(message);
+
+    var url = current_url.setQuery('act','dispWikiAdminContent').setQuery('module_srl','');
+    if(page) url = url.setQuery('page',page);
+    location.href = url;
+}
+
+
+/* Set Batch for bulk operations */
+function doCartSetup(url) {
+    var module_srl = new Array();
+    jQuery('#fo_list input[name=cart]:checked').each(function() {
+        module_srl[module_srl.length] = jQuery(this).val();
+    });
+
+    if(module_srl.length<1) return;
+
+    url += "&module_srls="+module_srl.join(',');
+    popopen(url,'modulesSetup');
+}
+
+function doArrangeWikiList(module_srl) {
+    exec_xml('wiki','procWikiAdminArrangeList',{module_srl:module_srl},function() {location.reload();});
+}
